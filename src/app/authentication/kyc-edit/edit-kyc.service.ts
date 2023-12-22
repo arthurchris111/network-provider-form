ecdf7228c7f718afe168c78c46fd3122fecdc3df
Christian <arthurchristian111@gmail.com>
2023-12-22 18:39:11 +0000

    changes

--- src/app/authentication/kyc-edit/edit-kyc.service.ts
+++ src/app/authentication/kyc-edit/edit-kyc.service.ts
@@ -22,20 +22,16 @@ export class EditKycService {
   public getOne(id: string): Observable<any> {
     return this.http.get(`/${id}/`, {
       headers: {
-        Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiYmYyNzNjYTItOTc3Mi00Njg3LTllZGItNGNhZjcyNDhmODAxIiwiZW1haWwiOiJtem5lZXJhaEBnbWFpbC5jb20iLCJ1c2VybmFtZSI6Im9sYSIsImV4dHJhX2luZm8yMzQ1MzAiOnRydWUsInVzZXJfcmVmZXJlbmNlIjoiY2E1NGE4YWM1OTNlZDZlMiIsImV4cCI6MTcwMzMyMTMwNSwiMmZhIjp0cnVlLCJvcmlnX2lhdCI6MTcwMzIzNDkwNSwib3RwX2RldmljZV9pZCI6Im90cF90b3RwLnRvdHBkZXZpY2UvMTQifQ.il_USwGc7X0tA1lQfyX9C40xU2hgp-aDO4216F-ELgc`,
+        Authorization: `Bearer `,
       },
     });
   }
 
   public putOne(id: string, data: any): Observable<any> {
-    return this.http.put(
-      `https://dca.revadeep.xyz/api/v1/admin/kyc_aml_record/${id}/`,
-      data,
-      {
-        headers: {
-          Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.seyJ1c2VyX2lkIjoiYmYyNzNjYTItOTc3Mi00Njg3LTllZGItNGNhZjcyNDhmODAxIiwiZW1haWwiOiJtem5lZXJhaEBnbWFpbC5jb20iLCJ1c2VybmFtZSI6Im9sYSIsImV4dHJhX2luZm8yMzQ1MzAiOnRydWUsInVzZXJfcmVmZXJlbmNlIjoiY2E1NGE4YWM1OTNlZDZlMiIsImV4cCI6MTcwMzMyMTMwNSwiMmZhIjp0cnVlLCJvcmlnX2lhdCI6MTcwMzIzNDkwNSwib3RwX2RldmljZV9pZCI6Im90cF90b3RwLnRvdHBkZXZpY2UvMTQifQ.il_USwGc7X0tA1lQfyX9C40xU2hgp-aDO4216F-ELgc`,
-        },
-      }
-    );
+    return this.http.put(`/${id}/`, data, {
+      headers: {
+        Authorization: `Bearer `,
+      },
+    });
   }
 }
